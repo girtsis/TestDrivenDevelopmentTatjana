409a699c22d299ce41402416c5cb220e7840312e
Daniel <girtsis.daniel@gmail.com>
2023-06-18 12:13:49 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const validatePassword = require('./validatePassword')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/users', (req, res) => {
+  const { username, password } = req.body
+
+  const validUsername = true
+  const validPassword = validatePassword(password)
+
+  if (validUsername && validPassword) {
+    res.send({message: "Valid User"})
+  } else {
+    res.send({error: "Invalid User"})
+  }
+})
+
+
+app.listen(3000, () => console.log("Listening on port 3000"))
